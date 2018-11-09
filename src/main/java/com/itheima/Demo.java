dba68ea01cd101dac73b084e23b0b71aee70d3f4
Vincent <vincent@itcast.cn>
2018-11-09 11:24:28 +0800

    Windows修改

--- src/main/java/com/itheima/Demo.java
+++ src/main/java/com/itheima/Demo.java
@@ -1,4 +1,4 @@
-package com.itheima;
+﻿package com.itheima;
 
 import com.sun.org.apache.xpath.internal.SourceTree;
 
@@ -6,6 +6,8 @@ public class Demo {
 
     public static void main(String[] args) {
         System.out.println("哒哒");
-    }
+         System.out.println("嘭嘭嘭windows");  
+
+  }
 
 }
